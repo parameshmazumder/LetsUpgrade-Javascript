e9b9675e858eee4030a977b67b7406981fcc3c15
parameshmazumder <62589162+parameshmazumder@users.noreply.github.com>
2020-09-11 16:33:58 +0530

    Add files via upload

---
+++ JS ASSIGNMENT DAY  4/Answer 1/Answer 1.js
@@ -0,0 +1,24 @@
+function button1(){
+    console.log("but 1");
+    const link3= document.getElementById("image");
+    link3.src= "https://cdn.vox-cdn.com/thumbor/4lcShxCTWmtzM3lf5Yq5v2IUNdg=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19858273/LogoVersion_Beta_Key_Art_VALORANT.jpg";
+}
+
+
+
+
+function button2(){
+    console.log("but 2");
+    const link2 = document.getElementById("image");
+    link2.src="https://www.denofgeek.com/wp-content/uploads/2020/08/Valorant.png?resize=768%2C432";
+}
+
+
+
+
+
+function button3(){
+    console.log("but 3");
+    const link3= document.getElementById("image");
+    link3.src= "https://media.altchar.com/prod/images/940_530/gm-2412e749-dfee-43c1-8c09-5cfbae066b7e-valorant-phoenix.jpg";
+}
